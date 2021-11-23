a1cad5e24c1713e32e2262fe80ac11579b369c10
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-11-22 20:34:44 -0700

    ReadME

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,8 @@ function generateTimeblock (hour, value) {
     timeBlockRow.addClass("row justify-content-center border-bottom border-light");
 
 var timeBlockHour = $("<div>")
-timeBlockHour.addClass("col-2 ml-2 bg-info text-light text-center p-2");
+timeBlockHour.addClass("col-2 ml-2 text-center p-2");
+timeBlockHour.addClass("hour time-block")
 if (hour < 12) {
     timeBlockHour.text(hour%12+"AM")
 }
@@ -20,31 +21,33 @@ console.log(hour%12)
 var timeBlockDetail = $("<div>")
 timeBlockDetail.addClass("col-6 border-right border-left border-light text-light p-2");
 if(hour < moment().hour()) {
-    timeBlockDetail.css("background-color", "orange")
+    timeBlockDetail.addClass("past")
     console.log("past")
 }
 else if(hour > moment().hour()) {
-    timeBlockDetail.css("future")
+    timeBlockDetail.addClass("future")
     console.log("future")
 }
 else {
-    timeBlockDetail.css("present")
-    console.log("current")
+    timeBlockDetail.addClass("present")
+    console.log("present")
 }
 
 var timeBlockInput = $("<textArea>")
 timeBlockInput.attr("type", "text")
 timeBlockInput.val(value)
 timeBlockDetail.append(timeBlockInput)
+timeBlockInput.css("width", "100%")
 if (localStorage.getItem(hour)) {
     timeBlockInput.val(localStorage.getItem(hour))
 }
 
 var timeBlockSave = $("<div>")
-timeBlockSave.addClass("col-2 mr-2 bg-success p-2");
+timeBlockSave.addClass("col-2 mr-2 p-2 saveBtn");
 var saveIcon =$("<i>")
-saveIcon.addClass("fas fa-save fa-4x justify-content-center")
+saveIcon.addClass("fas fa-save fa-3x")
 timeBlockSave.append(saveIcon)
+// timeBlockSave.css("justify-content-center")
 timeBlockSave.click(function (){
     // if(click.keyCode===13);
     console.log(timeBlockInput.val())
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Challenge-5-workday
+
+## Description
+This project was created so that a user can plan their workday beginning at 10am and ending at 6pm. When a user enters a task it is saved in local storage so that when they reload the page the task will also be loaded. The hours are color coded to indicate the past, present, and future. 
+
+This project utilizes moment, local Storage, JavaScript, Jquery, and Bootstrap. I learned how to style using bootstrap and jquery along with using moment to define time.
+## Installation
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+## Usage
+Provide instructions and examples for use. Include screenshots as needed.
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+## Credits
+T.A. Eric Sayer and tutor Armando Osario
+## License
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
